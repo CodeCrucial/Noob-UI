88d51a62e6202ac33c0dd3a0f834507f68b6b6be
Vault <jamal123sammy@icloud.com>
2025-12-28 22:09:45 -0500

    Update ThemeTag properties in main.lua

--- main.lua
+++ main.lua
@@ -3531,11 +3531,12 @@ Size=UDim2.new(1,0,1,0),
 ImageTransparency=.97,
 }),
 ab.NewRoundFrame(aj,"Glass-1.4",{
-ThemeTag={
-ImageColor3="Outline",
-},
-Size=UDim2.new(1,0,1,0),
-ImageTransparency=.75,
+    ThemeTag={
+        ImageColor3="TabBorder",
+        ImageTransparency="TabBorderTransparency"
+    },
+    Size=UDim2.new(1,0,1,0),
+    ImageTransparency=.75,
 },{
 
 
